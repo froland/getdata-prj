ebb7cbb1b1d729c853fde1707edecab9bc28020e
Francois ROLAND <fr.roland@gmail.com>
2015-07-24 15:10:45 +0200

    Moved algorithm into subroutines

--- get_data.R
+++ get_data.R
@@ -1,4 +1,4 @@
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zipFile <- "getdata-projectfiles-UCI HAR Dataset.zip"
-download.file(url, zipFile)
+download.file(url, zipFile, mode = "wb")
 unzip(zipFile)
--- merge_data.R
+++ merge_data.R
@@ -1,29 +1,41 @@
 require(dplyr)
 require(reshape2)
 
-features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE, col.names = c("index", "label"))
-features <- mutate(features, include = grepl(".*-mean\\(\\)", label) | grepl(".*-std\\(\\)", label), xlass = ifelse(include, "numeric", "NULL"))
-
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE, col.names = c("id", "label"), colClasses = c("integer", "factor"))
-
-merge_files <- function(filename_x, filename_y, filename_subjects) {
-        file_set <- read.table(filename_x,  colClasses = features$xlass, col.names = features$label, stringsAsFactors = FALSE)
-        file_subjects <- read.table(filename_subjects, stringsAsFactors = FALSE, col.names = c("subject"))
-        file_labels <- read.table(filename_y, col.names = c("activity"), stringsAsFactors = FALSE)
-        
-        file_set <- cbind(activity_id = file_labels$activity, subject = file_subjects$subject, file_set)
-
-        file_set %>%
-                inner_join(activity_labels, by = c(activity_id = "id")) %>%
-                rename(activity = label) %>%
-                select(-activity_id)
+read_features <- function() {
+        read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE, col.names = c("index", "label"))
 }
 
-training_set <- merge_files("UCI HAR Dataset/train/X_train.txt", "UCI HAR Dataset/train/y_train.txt", "UCI HAR Dataset/train/subject_train.txt")
-test_set <- merge_files("UCI HAR Dataset/test/X_test.txt", "UCI HAR Dataset/test/y_test.txt", "UCI HAR Dataset/test/subject_test.txt")
-all_set <- rbind(training_set, test_set)
-included_features <- filter(features, include)
-melted_set <- melt(all_set, id = c("activity", "subject"), variable.name = "measure", value.name = "value")
-by_activity_subject <- group_by(melted_set, activity, subject, measure)
-summary_set <- summarise(by_activity_subject, mean(value))
+concatenate_sets <- function(features) {
+        merge_files <- function(filename_x, filename_y, filename_subjects) {
+                classes <- rep("numeric", length = nrow(features))
+                file_set <- read.table(filename_x, col.names = features$label, colClasses = classes, stringsAsFactors = FALSE)
+                file_subjects <- read.table(filename_subjects, stringsAsFactors = FALSE, col.names = c("subject"))
+                file_labels <- read.table(filename_y, col.names = c("activity"), stringsAsFactors = FALSE)
+                
+                file_set <- cbind(activity_id = file_labels$activity, subject = file_subjects$subject, file_set)
+
+                activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("id", "label"), colClasses = c("integer", "factor"), stringsAsFactors = FALSE)
+                
+                file_set %>%
+                        inner_join(activity_labels, by = c(activity_id = "id")) %>%
+                        rename(activity = label) %>%
+                        select(-activity_id)
+        }
+        
+        training_set <- merge_files("UCI HAR Dataset/train/X_train.txt", "UCI HAR Dataset/train/y_train.txt", "UCI HAR Dataset/train/subject_train.txt")
+        test_set <- merge_files("UCI HAR Dataset/test/X_test.txt", "UCI HAR Dataset/test/y_test.txt", "UCI HAR Dataset/test/subject_test.txt")
+        rbind(training_set, test_set)
+}
+
+summarise_set <- function(to_summarise) {
+        to_summarise %>%
+                select(activity, subject, contains(".mean.."), contains(".std..")) %>%
+                melt(id = c("activity", "subject")) %>%
+                group_by(activity, subject, variable) %>%
+                summarise(mean(value))
+}
+
+features <- read_features()
+full_set <- concatenate_sets(features)
+summary_set <- summarise_set(full_set)
 write.table(summary_set, file = "summary.txt", row.names = FALSE)
